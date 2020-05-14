b45f5b7bac4be8d160a67452b6c3b08a71d9cc6d
CaitlinLandrus <50267367+CaitlinLandrus@users.noreply.github.com>
2020-05-14 16:07:59 -0500

    Comments on the methods

--- PetDatabase/src/main/java/com/csc422/petdatabase/Pet.java
+++ PetDatabase/src/main/java/com/csc422/petdatabase/Pet.java
@@ -14,31 +14,57 @@ public class Pet {
     private String name = "";
     private int age = 0;
     
+    /** No Arg Constructor **/
     public Pet(){
         
     }
     
+    /**
+     *  Constructor 
+     * @param name of pet
+     * @param age  of pet
+     */
     public Pet(String name, int age){
         this.name = name;
         this.age = age;
     }
      
+    /** 
+     * Sets the pet's age
+     * @param age 
+     */
     public void setAge(int age){
         this.age = age;
     }
     
+    /**
+     * Sets the pet's name
+     * @param name 
+     */
     public void setName(String name){
         this.name = name;
     }
         
+    /**
+     * 
+     * @return the pet's name
+     */
     public String getName(){
         return name;
     }
     
+    /**
+     * 
+     * @return the pet's age
+     */
     public int getAge(){
         return age;
     }  
     
+    /**
+     *  
+     * @return a string representation of the pet.
+     */
     @Override
     public String toString(){
         return name + " " + age;
